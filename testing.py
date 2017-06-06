ee5a01be32b842e7ae4869b2ee2f04bcaf9658d4
Jordan Prudhomme <jkprudhomme@brandeis.edu>
2017-06-06 14:59:59 -0700

    Redid potato changes

--- testing.py
+++ testing.py
@@ -1,10 +1,7 @@
 #some code about things
 
-<<<<<<< HEAD
-print("Hello", "World", end = !/n)
+print ("Hello", "World" , end = '!\n')
 
 for i in range(10)
 	print ("I have %d potatos", % i)
-=======
-print ("Hello", "World" , end = '!\n')
->>>>>>> master
+
