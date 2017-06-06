d1ab87fde10e02bb65f03b0106a32458cc58efe4
Jordan Prudhomme <jkprudhomme@brandeis.edu>
2017-06-06 16:15:05 -0700

    Fished shopping list

--- testing2.py
+++ testing2.py
@@ -1,1 +1,1 @@
-shoppingList = {"produce": ["iceberg lettuce", "arugula", "carrots", "beets"], "meat":  , "dairy":  ,"carbs": }
+shoppingList = {"produce": ["iceberg lettuce", "arugula", "carrots", "beets"], "meat": ["baby back ribs", "carne asada"] , "dairy":  ["Milk", "Pepper Jack Cheese", "Cheddar"],"carbs": ["Bagels", "Whole Wheat Bread"]}
